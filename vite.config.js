6b84f72c7e88822ee7a0c18f858207622a52233c
DiegoLlashag <diego.llashag.antonio1298@gmail.com>
2025-10-24 16:43:34 -0500

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/blackjack-vite/",
+  //se pueden añadir más configuraciones aquí, es un objeto con varias propiedades como:
+//   server: {
+//     port: 3000
+//   }
+});
