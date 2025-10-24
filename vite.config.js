2a418d461f91628dcf3f120a3f51b6697bea27da
DiegoLlashag <diego.llashag.antonio1298@gmail.com>
2025-10-24 16:48:20 -0500

    add real name base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/blackjack-vite/",
+  base: "/js-blackjack-vite/", //el nombre del proyecto de github
   //se pueden añadir más configuraciones aquí, es un objeto con varias propiedades como:
 //   server: {
 //     port: 3000
